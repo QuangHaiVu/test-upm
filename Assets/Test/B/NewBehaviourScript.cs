1e91facf3c0538516ba6943d5e6eea8fd5ceaa43
QuangHaiVu <40314916+QuangHaiVu@users.noreply.github.com>
2022-08-11 00:33:10 +0700

    fix: test CI

--- Assets/Test/B/NewBehaviourScript.cs
+++ Assets/Test/B/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("A");
     }
 
     // Update is called once per frame
